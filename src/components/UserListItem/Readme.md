4a60bdc43c350552e685754d86e8b39d6cc31e17
James Canning <james@brudil.com>
2017-11-21 10:20:53 +0000

    Remove unused component

--- src/components/UserListItem/Readme.md
+++
@@ -1,1 +0,0 @@
-UserListItem example:
--- src/components/UserListItem/UserListItem.css.d.ts
+++
@@ -1,5 +0,0 @@
-export const root: string;
-export const root_removable: string;
-export const avatar: string;
-export const name: string;
-export const handle: string;
